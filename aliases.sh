c38555baf4ad685333338d72cd0face2baa7a0dd
Eugene Sinamban <eugene.sinamban@gmail.com>
2021-05-08 22:08:20 +0900

    add backup scripts

--- aliases.sh
+++ aliases.sh
@@ -14,6 +14,59 @@ alias reshub-lcl-client-logs="docker-compose logs -f --tail 100 client"
 alias reshub-lcl-restart="docker-compose restart server client"
 alias reshub-lcl-server-bash="docker-compose exec server bash"
 alias reshub-lcl-client-bash="docker-compose exec client bash"
+alias reshub-lcl-db-bash="docker-compose exec db bash"
+
+# db back up
+function reshub-db-backup() {
+  if [ -z "$1" ]; then
+    echo "バックアップのフォルダー名をお願いします"
+    echo "Please enter back up data folder name"
+  else
+    echo "コンテナを停止します"
+    echo "Stopping containers"
+    docker stop reshub-server reshub_mongodb
+
+    echo "バックアップ開始";
+    echo "Back up starting..."
+    docker run --rm -ti --volumes-from reshub_mongodb -v $(pwd):/backup ubuntu tar cvf /backup/$1.tar /data/db
+
+    if [ $? -eq 0 ]; then
+      echo "バックアップ完了"
+      echo "Back up saved"
+      echo $1
+    else
+      echo "エラーが起きました"
+      echo "Error occured"
+    fi
+  fi
+}
+
+# db restore
+function reshub-db-restore() {
+  if [ -z "$1" ]; then
+    echo "バックアップのファイルー名をお願いします"
+    echo "Please enter back up data file name"
+  elif [ ! -e "$1.tar" ]; then
+    echo "ファイル名が間違っています"
+    echo "Please enter correct file name"
+  else
+    echo "コンテナを停止します"
+    echo "Stopping containers"
+    docker stop reshub-server reshub_mongodb
+
+    echo "修復開始"
+    echo "Restoring database..."
+    docker run --rm --volumes-from reshub_mongodb -v $(pwd):/backup mongo:4.2 bash -c "cd /data && tar xvf /backup/$1.tar --strip 1 && mongod --repair"
+
+    if [ $? -eq 0 ]; then
+      echo "修復完了"
+      echo "Restoration complete"
+    else
+      echo "エラーが起きました"
+      echo "Error occured"
+    fi
+  fi
+}
 
 # prd
 alias reshub-prd="reshub-prd-build && docker-compose up -d prd"
