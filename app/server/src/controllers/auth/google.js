23cd2fd622f68c7396d503d0b5b85737a3b8cf0e
Eugene Sinamban <eugene.sinamban@gmail.com>
2021-06-15 17:07:21 +0900

    add twitter authentication

--- app/server/src/controllers/auth/google.js
+++ app/server/src/controllers/auth/google.js
@@ -2,21 +2,20 @@ const router = require('express').Router()
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 const { User } = require('../../models/user')
-const { login } = require('./utils')
+const { login, passportData } = require('./utils')
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: `${process.env.RESHUB_URL}/auth/google/callback`,
 }, async (accessToken, refreshToken, profile, done) => {
-  let user = await User.findOne({googleID: profile.id}).or([{email: profile.emails[0].value}]).exec()
-  if (!user) return done({code: 404, message: "User not found", data: {
-    firstName: profile.name.givenName,
-    lastName: profile.name.familyName,
-    email: profile.emails[0].value,
-    googleID: profile.id,
-  }})
-  return done(null, user)
+  try {
+    let user = await User.findOne({googleID: profile.id}).or([{email: profile.emails[0].value}]).exec()
+    if (!user) return done({ code: 404, message: "User not found", data: passportData(profile) })
+    return done(null, user)
+  } catch (e) {
+    done(e, null)
+  }
 }))
 
 router.get('/', passport.authenticate('google', { scope: ['email', 'profile']}))
