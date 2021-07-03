f9843617c30dce5dd6fd70efac28c94600bbd7ad
sinhyez <upthe15752@gmail.com>
2021-07-03 14:47:27 +0900

    add shop action and reducer

---
+++ app/client/src/store/types/authTypes.js
@@ -0,0 +1,11 @@
+//----------------------------------
+// redux action types ユーザー 
+//----------------------------------
+
+
+// // ユーザーのリクエストをスタートする
+// export const USER_REQUEST_START = 'USER_REQUEST_START'
+// // ユーザーのリクエストが成功した場合
+// export const USER_REQUEST_SUCCESS = 'USER_REQUEST_SUCCESS' 
+// // ユーザーのリクエストが失敗した場合
+// export const USER_REQUEST_FAILURE = 'USER_REQUEST_FAILURE'
