222c0ab3906a546f958a4583d9e3b60043938816
sinhyez <upthe15752@gmail.com>
2021-07-13 14:47:38 +0900

    add google login

--- app/client/src/store/reducers/rootReducer.js
+++ app/client/src/store/reducers/rootReducer.js
@@ -5,8 +5,14 @@
 import { combineReducers } from 'redux'
 import { authReducer } from './authReducer'
 import { shopReducer } from './shopReducer'
+import { areaReducer } from './areaReducer'
+import { prefectureReducer } from './prefectureReducer'
+import { citiesReducer } from './citiesReducer'
 
 export const rootReducer = combineReducers({
-    // auth: authReducer,
-    shop: shopReducer
+    auth: authReducer,
+    shop: shopReducer,
+    area: areaReducer,
+    prefecture: prefectureReducer,
+    citie: citiesReducer
   })
