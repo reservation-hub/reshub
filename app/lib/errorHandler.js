ac7d862412179e145e3b61dec8435f32f20d0aa6
Eugene Sinamban <eugene.sinamban@gmail.com>
2021-07-23 22:19:27 +0900

    fix auth error handling

--- app/lib/errorHandler.js
+++ app/lib/errorHandler.js
@@ -3,11 +3,12 @@ const { Prisma } = require('@prisma/client')
 // eslint-disable-next-line no-unused-vars
 exports.errorHandler = (err, req, res, next) => {
   console.error('error: ', err)
+  const { error, message, code } = err
 
   // prisma errors
-  if (err.error && err.error instanceof Prisma.PrismaClientKnownRequestError) {
-    if (err.error.code === 'P2002') {
-      const invalidFields = err.error.meta.target
+  if (error && error instanceof Prisma.PrismaClientKnownRequestError) {
+    if (error.code === 'P2002') {
+      const invalidFields = error.meta.target
       return res.status(400).send({
         error: invalidFields.map(field => ({
           name: field,
@@ -15,29 +16,29 @@ exports.errorHandler = (err, req, res, next) => {
         })),
       })
     }
-    return res.status(400).send({ error: { message: err.message } })
+    return res.status(400).send({ error: { message } })
   }
 
-  if (err.name === 'ValidationError') {
+  if (error.name === 'ValidationError') {
     // Joi Validation エラー処理
-    const messages = err.details.map(e => ({
+    const messages = error.details.map(e => ({
       label: e.context.label,
       message: e.message,
     }))
     return res.status(400).send({ error: { message: messages } })
   }
 
-  if (err.code === 404) {
-    return res.status(err.code).send({ error: { message: err.message || 'Error: Not Found' } })
+  if (code === 404) {
+    return res.status(code).send({ error: { message: message || 'Error: Not Found' } })
   }
 
-  if (err.code === 401) {
-    return res.status(err.code).send({ error: { message: err.message || 'Error: Bad request' } })
+  if (code === 401) {
+    return res.status(code).send({ error: { message: message || 'Error: Bad request' } })
   }
 
-  if (err.name === 'JsonWebTokenError') {
-    return res.status(400).send({ error: { message: err.message } })
+  if (error.name === 'JsonWebTokenError') {
+    return res.status(400).send({ error: { message } })
   }
 
-  return res.status(err.code || 500).send({ error: { message: err.message || 'Internal Server Error', data: err.data } })
+  return res.status(code || 500).send({ error: { message: message || 'Internal Server Error' } })
 }
