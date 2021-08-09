fba1f997630e058fd97d3d06299b7cea8ef40922
Eugene Sinamban <eugene.sinamban@gmail.com>
2021-08-09 23:51:32 +0900

    insert shop schedule

---
+++ app/prisma/migrations/20210809143218_add_shop_schedule_json/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "ShopDetail" ADD COLUMN     "schedule" JSONB;
--- app/src/controllers/schemas/shop.ts
+++ app/src/controllers/schemas/shop.ts
@@ -8,3 +8,11 @@ export const shopUpsertSchema = Joi.object({
   prefectureId: Joi.number().integer().required(),
   cityId: Joi.number().integer().required(),
 })
+
+export const shopScheduleSchema = Joi.object({
+  days: Joi.array().items(Joi.number().valid(0, 1, 2, 3, 4, 5, 6)).min(1).required(),
+  hours: Joi.object({
+    start: Joi.string().pattern(/^(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)$/).required(),
+    end: Joi.string().pattern(/^(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)$/).required(),
+  }),
+})
