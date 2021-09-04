7cf0473426d7010960327d0c759ea05f09c490df
Eugene Sinamban <eugene.sinamban@gmail.com>
2021-09-04 21:47:45 +0900

    code clean up

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     },
   },
   rules: {
+    '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'arrow-parens': ['error', 'as-needed'],
     'arrow-body-style': 'error',
