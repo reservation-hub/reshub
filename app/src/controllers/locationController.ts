f1a1c366ee2b65d133d6dfa7c6306f536bae5027
Eugene Sinamban <eugene.sinamban@gmail.com>
2021-09-06 16:18:07 +0900

    add show endpoint for locations

--- app/src/controllers/locationController.ts
+++ app/src/controllers/locationController.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express'
 import asyncHandler from 'express-async-handler'
-import { roleCheck } from '../routes/utils'
+import { parseIntIdMiddleware, roleCheck } from '../routes/utils'
 import LocationService, { LocationQuery, LocationResponse } from '../services/LocationService'
 import indexSchema from './schemas/indexSchema'
+import { Area, Prefecture, City } from '../entities/Location'
 
 export type LocationServiceInterface = {
   fetchAreasWithCount(query: LocationQuery): Promise<LocationResponse>,
+  fetchArea(id: number): Promise<Area>,
   fetchPrefecturesWithCount(query: LocationQuery): Promise<LocationResponse>,
+  fetchPrefecture(id: number): Promise<Prefecture>,
   fetchCitiesWithCount(query: LocationQuery): Promise<LocationResponse>,
+  fetchCity(id: number): Promise<City>,
 }
 
 const joiOptions = { abortEarly: false, stripUnknown: true }
@@ -18,10 +22,22 @@ const areaIndex = asyncHandler(async (req, res) => {
   return res.send(value)
 })
 
+const showArea = asyncHandler(async (req, res) => {
+  const { id } = res.locals
+  const area = await LocationService.fetchArea(id)
+  return res.send(area)
+})
+
 const prefectureIndex = asyncHandler(async (req, res) => {
   const params = await indexSchema.validateAsync(req.query, joiOptions)
   const value = await LocationService.fetchPrefecturesWithCount(params)
   return res.send(value)
+})
+
+const showPrefecture = asyncHandler(async (req, res) => {
+  const { id } = res.locals
+  const prefecture = await LocationService.fetchPrefecture(id)
+  return res.send(prefecture)
 })
 
 const cityIndex = asyncHandler(async (req, res) => {
@@ -30,6 +46,21 @@ const cityIndex = asyncHandler(async (req, res) => {
   return res.send(value)
 })
 
-export const areaController = Router().get('/', roleCheck(['admin']), areaIndex)
-export const prefectureController = Router().get('/', roleCheck(['admin']), prefectureIndex)
-export const cityController = Router().get('/', roleCheck(['admin']), cityIndex)
+const showCity = asyncHandler(async (req, res) => {
+  const { id } = res.locals
+  const city = await LocationService.fetchCity(id)
+  return res.send(city)
+})
+
+export const areaController = Router()
+export const prefectureController = Router()
+export const cityController = Router()
+
+areaController.get('/', roleCheck(['admin']), areaIndex)
+areaController.get('/:id', roleCheck(['admin']), parseIntIdMiddleware, showArea)
+
+prefectureController.get('/', roleCheck(['admin']), prefectureIndex)
+prefectureController.get('/:id', roleCheck(['admin']), parseIntIdMiddleware, showPrefecture)
+
+cityController.get('/', roleCheck(['admin']), cityIndex)
+cityController.get('/:id', roleCheck(['admin']), parseIntIdMiddleware, showCity)
--- app/src/services/LocationService.ts
+++ app/src/services/LocationService.ts
@@ -4,6 +4,7 @@ import { Area, Prefecture, City } from '../entities/Location'
 import {
   AreaRepository, CityRepository, PrefectureRepository,
 } from '../repositories/LocationRepository'
+import { NotFoundError } from './Errors/ServiceError'
 
 export type LocationQuery = {
   page: number,
@@ -13,28 +14,50 @@ export type LocationQuery = {
 
 export type LocationResponse = { data: Area[] | Prefecture[] | City[], totalCount: number }
 
-export const fetchAreasWithCount = async (query: LocationQuery) : Promise<LocationResponse> => {
-  const areaCount = await AreaRepository.totalCount()
-  const areas = await AreaRepository.fetchAll(query)
-  return { data: areas, totalCount: areaCount }
-}
-
-export const fetchPrefecturesWithCount = async (query: LocationQuery) : Promise<LocationResponse> => {
-  const prefectureCount = await PrefectureRepository.totalCount()
-  const prefectures = await PrefectureRepository.fetchAll(query)
-  return { data: prefectures, totalCount: prefectureCount }
-}
-
-export const fetchCitiesWithCount = async (query: LocationQuery) : Promise<LocationResponse> => {
-  const cityCount = await CityRepository.totalCount()
-  const cities = await CityRepository.fetchAll(query)
-  return { data: cities, totalCount: cityCount }
-}
-
 const LocationService: LocationServiceInterface = {
-  fetchAreasWithCount,
-  fetchPrefecturesWithCount,
-  fetchCitiesWithCount,
+
+  async fetchAreasWithCount(query) {
+    const areaCount = await AreaRepository.totalCount()
+    const areas = await AreaRepository.fetchAll(query)
+    return { data: areas, totalCount: areaCount }
+  },
+
+  async fetchArea(id) {
+    const area = await AreaRepository.fetch(id)
+    if (!area) {
+      throw new NotFoundError()
+    }
+    return area
+  },
+
+  async fetchPrefecturesWithCount(query) {
+    const prefectureCount = await PrefectureRepository.totalCount()
+    const prefectures = await PrefectureRepository.fetchAll(query)
+    return { data: prefectures, totalCount: prefectureCount }
+  },
+
+  async fetchPrefecture(id) {
+    const prefecture = await PrefectureRepository.fetch(id)
+    if (!prefecture) {
+      throw new NotFoundError()
+    }
+    return prefecture
+  },
+
+  async fetchCitiesWithCount(query) {
+    const cityCount = await CityRepository.totalCount()
+    const cities = await CityRepository.fetchAll(query)
+    return { data: cities, totalCount: cityCount }
+  },
+
+  async fetchCity(id) {
+    const city = await CityRepository.fetch(id)
+    if (!city) {
+      throw new NotFoundError()
+    }
+    return city
+  },
+
 }
 
 export default LocationService
