5a66da5dd75d082f61b9727fc9ed494937f099f7
Eugene Sinamban <eugene.sinamban@gmail.com>
2021-11-10 01:35:43 +0900

    fix client auth controller

--- app/src/client/controllers/authController.ts
+++ app/src/client/controllers/authController.ts
@@ -1,7 +1,6 @@
 import {
-  Router, CookieOptions, Request, Response,
+  Router, CookieOptions, Request, Response, NextFunction,
 } from 'express'
-import asyncHandler from 'express-async-handler'
 import AuthService from '../services/AuthService'
 import { UnknownServerError } from '../../routes/errors'
 import passport from '../../middlewares/passport'
@@ -12,38 +11,43 @@ export type AuthServiceInterface = {
   verifyIfUserInTokenIsLoggedIn(authToken: any, headerToken?: string): Promise<void>
 }
 
-const login = asyncHandler(async (req, res) => {
-  const { user } = req
-  if (!user) {
-    throw new UnknownServerError()
-  }
+const login = async (req: Request, res: Response, next: NextFunction) : Promise<Response | void> => {
+  try {
+    const { user } = req
+    if (!user) {
+      throw new UnknownServerError()
+    }
 
-  // トークン生成
-  const cookieOptions: CookieOptions = {
-    httpOnly: config.NODE_ENV === 'production',
-    secure: config.NODE_ENV === 'production',
-    sameSite: config.NODE_ENV === 'production' ? 'none' : 'lax',
-    maxAge: 1000 * 60 * 60 * 12,
-    signed: true,
-  }
-  const token = AuthService.createToken(user)
+    // トークン生成
+    const cookieOptions: CookieOptions = {
+      httpOnly: config.NODE_ENV === 'production',
+      secure: config.NODE_ENV === 'production',
+      sameSite: config.NODE_ENV === 'production' ? 'none' : 'lax',
+      maxAge: 1000 * 60 * 60 * 12,
+      signed: true,
+    }
+    const token = AuthService.createToken(user)
 
-  // クッキーを設定
-  res.cookie('authToken', token, cookieOptions)
-  res.send({ user, token })
-})
+    // クッキーを設定
+    res.cookie('authToken', token, cookieOptions)
+    return res.send({ user, token })
+  } catch (e) { return next(e) }
+}
 
-export const verifyIfNotLoggedInYet = asyncHandler(async (req, res, next) => {
-  const { signedCookies } = req
+export const verifyIfNotLoggedInYet = async (req: Request, res: Response, next: NextFunction)
+  : Promise<Response | void> => {
+  try {
+    const { signedCookies } = req
 
-  if (!signedCookies || !signedCookies.authToken) return next()
-  let headerToken
-  if (req.get('authorization')) {
-    headerToken = req.get('authorization')?.split(' ')[1]
-  }
-  await AuthService.verifyIfUserInTokenIsLoggedIn(signedCookies.authToken, headerToken)
-  return next()
-})
+    if (!signedCookies || !signedCookies.authToken) return next()
+    let headerToken
+    if (req.get('authorization')) {
+      headerToken = req.get('authorization')?.split(' ')[1]
+    }
+    await AuthService.verifyIfUserInTokenIsLoggedIn(signedCookies.authToken, headerToken)
+    return next()
+  } catch (e) { return next(e) }
+}
 
 export const logout = (req: Request, res: Response): void => {
   res.clearCookie('authToken')
