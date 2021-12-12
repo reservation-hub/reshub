f6f9a49c03d9097f6cbcf31a51f92dcbdd4fd1db
sabi1125 <sabir_barahi4@hotmail.com>
2021-12-12 21:52:18 +0900

    added stylist Schedule

--- app/src/controllers/schemas/stylist.ts
+++ app/src/controllers/schemas/stylist.ts
@@ -9,4 +9,7 @@ export const stylistUpsertSchema = Joi.object({
 export const shopStylistUpsertSchema = Joi.object({
   name: Joi.string().required(),
   price: Joi.number().min(0).required(),
+  days: Joi.array().items(Joi.number().valid(0, 1, 2, 3, 4, 5, 6)).min(1).required(),
+  startTime: Joi.string().pattern(/^(?:([01]?\d|2[0-3]):)?([0-5]?\d)$/).required(),
+  endTime: Joi.string().pattern(/^(?:([01]?\d|2[0-3]):)?([0-5]?\d)$/).required(),
 })
