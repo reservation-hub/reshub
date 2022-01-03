a38f4456cd3d30b0d7a84a18f0d97e0141e79611
Eugene Sinamban <eugene.sinamban@gmail.com>
2022-01-03 23:09:16 +0900

    add menu endpoint

--- app/src/controller-adapter/client/Shop.ts
+++ app/src/controller-adapter/client/Shop.ts
@@ -3,14 +3,20 @@ import {
 } from 'express'
 import {
   SalonListQuery, SalonListResponse, SalonQuery, SalonResponse,
+  SalonMenuListQuery, SalonMenuListResponse,
 } from '@request-response-types/client/Shop'
-import ShopController from '@client/shop/ShopController'
 import { UserForAuth } from '@entities/User'
-import { parseIntIdMiddleware } from '@/routes/utils'
+import { parseIntIdMiddleware } from '@routes/utils'
+import ShopController from '@client/shop/ShopController'
+import MenuController from '@client/menu/MenuController'
 
 export type ShopControllerInterface = {
   index(user: UserForAuth, query: SalonListQuery): Promise<SalonListResponse>
   detail(user: UserForAuth, query: SalonQuery): Promise<SalonResponse>
+}
+
+export type MenuControllerInterface = {
+  list(user: UserForAuth, query: SalonMenuListQuery): Promise<SalonMenuListResponse>
 }
 
 const index = async (req: Request, res: Response, next: NextFunction) : Promise<Response | void> => {
@@ -29,9 +35,24 @@ const detail = async (req: Request, res: Response, next: NextFunction) : Promise
   } catch (e) { return next(e) }
 }
 
+const shopMenus = async (req: Request, res: Response, next: NextFunction) : Promise<Response | void> => {
+  try {
+    const user = req.user as UserForAuth
+    const { shopId } = res.locals
+    const { page, order } = req.query
+    return res.send(await MenuController.list(user, { shopId, page, order }))
+  } catch (e) { return next(e) }
+}
+
 const routes = Router()
 
 routes.get('/', index)
 routes.get('/:shopId', parseIntIdMiddleware, detail)
+
+/**
+ * Menu routes
+ */
+
+routes.get('/:shopId/menus', parseIntIdMiddleware, shopMenus)
 
 export default routes
--- app/src/controllers/client/menu/MenuController.ts
+++ app/src/controllers/client/menu/MenuController.ts
@@ -1,15 +1,35 @@
+import { OrderBy } from '@entities/Common'
 import { Menu } from '@entities/Menu'
 import { UserForAuth } from '@entities/User'
-import { MenuControllerInterface } from '@controller-adapter/client/Menu'
 import MenuService from '@client/menu/services/MenuService'
+import { MenuControllerInterface as MenuSocket } from '@controller-adapter/client/Menu'
+import { MenuControllerInterface as ShopSocket } from '@controller-adapter/client/Shop'
+import { indexSchema } from './schemas'
 
 export type MenuServiceInterface = {
   popularMenus(user: UserForAuth): Promise<Menu[]>
+  fetchShopMenusWithTotalCount(user: UserForAuth, shopId: number, page: number, order: OrderBy)
+    :Promise<{ menus: Menu[], totalCount: number}>
 }
-
-const MenuController: MenuControllerInterface = {
+const joiOptions = { abortEarly: false, stripUnknown: true }
+const MenuController: MenuSocket & ShopSocket = {
   async popularMenus(user) {
     return MenuService.popularMenus(user)
+  },
+
+  async list(user, query) {
+    const { shopId } = query
+    const { page, order } = await indexSchema.validateAsync(query, joiOptions)
+    const { menus, totalCount } = await MenuService.fetchShopMenusWithTotalCount(user, shopId, page, order)
+    const values = menus.map(m => ({
+      id: m.id,
+      shopId: m.shopId,
+      name: m.name,
+      price: m.price,
+      duration: m.duration,
+      description: m.description,
+    }))
+    return { values, totalCount }
   },
 }
 
