77693346079b4334cd4004c08163f64d50ff794e
Eugene Sinamban <eugene.sinamban@gmail.com>
2022-01-10 01:07:37 +0900

    fix test

--- app/test.ts
+++ app/test.ts
@@ -1,13 +1,8 @@
 /* eslint-disable */
-import MenuRepository, { setPopularMenus } from '@client/menu/repositories/MenuRepository'
-
 (async () => {
 
   try {
-    await setPopularMenus(2022, 2)
-    const popularMenus = await MenuRepository.fetchPopularMenus(2022, 2)
-    console.log(popularMenus)
-
+    
   } catch (e) { console.error(e) }
   
   process.exit(0)
