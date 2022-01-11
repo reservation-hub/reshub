f5acecc1ecd4a495248ea94961e9d9a5bc45f9fc
Eugene Sinamban <eugene.sinamban@gmail.com>
2022-01-11 15:48:47 +0900

    add cors settings for client

--- app/src/app.ts
+++ app/src/app.ts
@@ -14,7 +14,7 @@ app.use(MorganMiddleware)
 app.use(express.json())
 app.use(cookieParser(config.JWT_TOKEN_SECRET))
 app.use(cors({
-  origin: 'http://localhost:8080',
+  origin: ['http://localhost:8080', 'http://localhost:3001'],
   credentials: true,
 }))
 app.use(helmet())
