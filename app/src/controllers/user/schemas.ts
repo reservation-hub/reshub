7aa90e57b7f5e09e6344a8b05a0fa7cbcc825004
Eugene Sinamban <eugene.sinamban@gmail.com>
2022-01-23 17:52:43 +0900

    fix user schema for zod

--- app/src/controllers/user/schemas.ts
+++ app/src/controllers/user/schemas.ts
@@ -1,50 +1,51 @@
-import joi from 'joi'
 import { RoleSlug } from '@request-response-types/models/Role'
 import { OrderBy } from '@request-response-types/Common'
+import { z } from 'zod'
+import { Gender } from '@request-response-types/models/User'
 
-export const indexSchema = joi.object({
-  page: joi.string().pattern(/^[0-9]+$/),
-  order: joi.string().valid(OrderBy.ASC, OrderBy.DESC),
+export const indexSchema = z.object({
+  page: z.number().optional(),
+  order: z.nativeEnum(OrderBy).optional(),
 })
 
-export const userInsertSchema = joi.object({
-  password: joi.string().trim().alphanum().required(),
-  confirm: joi.string().trim().alphanum().required(),
-  email: joi.string().email().trim().required(),
-  roleSlug: joi.string().valid(RoleSlug.ADMIN, RoleSlug.CLIENT, RoleSlug.SHOP_STAFF).required(),
-  firstNameKanji: joi.string().trim().required(),
-  lastNameKanji: joi.string().trim().required(),
-  firstNameKana: joi.string().trim().required(),
-  lastNameKana: joi.string().trim().required(),
-  gender: joi.string().valid('male', 'female').required(),
-  birthday: joi.string().pattern(/^[1-2][0-9]{3}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/).required(),
+export const userInsertSchema = z.object({
+  password: z.string().regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d~!@#$%^&*()+|=]{8,}$/),
+  confirm: z.string().regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d~!@#$%^&*()+|=]{8,}$/),
+  email: z.string().email(),
+  roleSlug: z.nativeEnum(RoleSlug),
+  firstNameKanji: z.string(),
+  lastNameKanji: z.string(),
+  firstNameKana: z.string(),
+  lastNameKana: z.string(),
+  gender: z.nativeEnum(Gender),
+  birthday: z.string().regex(/^[1-2][0-9]{3}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/),
 })
 
-export const userUpdateSchema = joi.object({
-  email: joi.string().email().trim().required(),
-  roleSlug: joi.string().valid(RoleSlug.ADMIN, RoleSlug.CLIENT, RoleSlug.SHOP_STAFF).required(),
-  firstNameKanji: joi.string().trim().required(),
-  lastNameKanji: joi.string().trim().required(),
-  firstNameKana: joi.string().trim().required(),
-  lastNameKana: joi.string().trim().required(),
-  gender: joi.string().valid('male', 'female').required(),
-  birthday: joi.string().pattern(/^[1-2][0-9]{3}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/).required(),
+export const userUpdateSchema = z.object({
+  email: z.string().email(),
+  roleSlug: z.nativeEnum(RoleSlug),
+  firstNameKanji: z.string(),
+  lastNameKanji: z.string(),
+  firstNameKana: z.string(),
+  lastNameKana: z.string(),
+  gender: z.nativeEnum(Gender),
+  birthday: z.string().regex(/^[1-2][0-9]{3}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/),
 })
 
-export const userPasswordUpdateSchema = joi.object({
-  oldPassword: joi.string().trim().alphanum().required(),
-  newPassword: joi.string().trim().alphanum().required(),
-  confirmNewPassword: joi.string().trim().alphanum().required(),
+export const userPasswordUpdateSchema = z.object({
+  oldPassword: z.string().regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d~!@#$%^&*()+|=]{8,}$/),
+  newPassword: z.string().regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d~!@#$%^&*()+|=]{8,}$/),
+  confirmNewPassword: z.string().regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d~!@#$%^&*()+|=]{8,}$/),
 })
 
-export const userOAuthIdUpsertSchema = joi.object({
-  googleId: joi.string().trim().allow('', null),
-  lineId: joi.string().trim().allow('', null),
-  twitterId: joi.string().trim().allow('', null),
+export const userOAuthIdUpsertSchema = z.object({
+  googleId: z.string().optional(),
+  lineId: z.string().optional(),
+  twitterId: z.string().optional(),
 })
 
-export const searchSchema = joi.object({
-  keyword: joi.string().required(),
-  page: joi.string().pattern(/^[0-9]+$/),
-  order: joi.string().valid(OrderBy.ASC, OrderBy.DESC),
+export const searchSchema = z.object({
+  keyword: z.string(),
+  page: z.number().positive().int().optional(),
+  order: z.nativeEnum(OrderBy).optional(),
 })
