39a49718cf66fd6f8fc9a137624caeb839b5fe54
Eugene Sinamban <eugene.sinamban@gmail.com>
2022-01-25 17:45:50 +0900

    add cors settings for prod

--- app/test.ts
+++ app/test.ts
@@ -1,12 +1,7 @@
 /* eslint-disable */
 (async () => {
   
-  try {
-    const dateString = '2022-01-22T17:00:00+09:00'
-    const date = new Date(dateString)
-    const localDate = date.toLocaleString('en-US', { timeZone: 'Asia/Tokyo', hour12: false })
-    console.log(localDate, date.toISOString())
-  } catch (e) { console.error(e) }
+  try {} catch (e) { console.error(e) }
   
   process.exit(0)
 })()
