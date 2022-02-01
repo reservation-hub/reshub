844068115d1f782a6b15aa5e3c915cbba63e417d
Eugene Sinamban <eugene.sinamban@gmail.com>
2022-02-01 11:45:27 +0900

    add tags to shops in client

--- app/src/controllers/client/shop/repositories/TagRepository.ts
+++ app/src/controllers/client/shop/repositories/TagRepository.ts
@@ -1,16 +1,29 @@
 import { Tag as PrismaTag } from '@prisma/client'
 import { Tag } from '@entities/Tag'
 import prisma from '@lib/prisma'
-import { TagRepositoryInterface } from '@client/shop/services/ShopService'
+import { TagRepositoryInterface as ShopServiceSocket } from '@client/shop/services/ShopService'
+import { TagRepositoryInterface as TagServiceSocket } from '@client/shop/services/TagService'
 
 const convertToEntityTag = (tag: PrismaTag): Tag => ({ id: tag.id, slug: tag.slug })
 
-const TagRepository: TagRepositoryInterface = {
+const TagRepository: ShopServiceSocket & TagServiceSocket = {
   async fetchValidTagsBySlugs(slugs) {
     const tags = await prisma.tag.findMany({
       where: { slug: { in: slugs } },
     })
     return tags.map(convertToEntityTag)
+  },
+
+  async fetchShopsTags(shopIds) {
+    const tags = await prisma.shopTags.findMany({
+      where: { shopId: { in: shopIds } },
+      include: { tag: true },
+    })
+
+    return shopIds.map(shopId => ({
+      shopId,
+      tags: tags.filter(t => (t.shopId === shopId)).map(t => t.tag),
+    }))
   },
 }
 
